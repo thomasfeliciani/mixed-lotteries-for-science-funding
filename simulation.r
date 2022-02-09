13109c65e7e4c9688fa5ac6b3a970a821bef8087
Thomas Feliciani <51531255+thomasfeliciani@users.noreply.github.com>
2022-02-09 17:12:55 +0100

    Implemented lottery types 0-4

--- simulation.r
+++ simulation.r
@@ -11,12 +11,14 @@ if (!dir.exists("./output/")) dir.create("./output/")
 
 
 nProposals = 10
-fundingRate = 0.5
-lotteryChoice = 0.3
+targetFundingRate = 0.5
+sufficientMerit = 0.6
+
+lotteryChoiceRate = 0.3
 #lotterySize = 0.5
 scaleGranularity = 5
-nReviewers = 3
-panelChoice = fundingRate - lotteryChoice # default
+nReviewers = 2
+#panelChoice = fundingRate - lotteryChoice # default
 panelGranularity = scaleGranularity * nReviewers - (nReviewers - 1)
 
 
@@ -25,108 +27,58 @@ panelGranularity = scaleGranularity * nReviewers - (nReviewers - 1)
 
 
 
-if (fundingRate != panelChoice + lotteryChoice) warning(
+if (targetFundingRate != panelChoice + lotteryChoice) warning(
   "panelChoice and lotteryChoice don't add up to the fundingRate."
 )
 
 
 p <- data.frame(
   won = rep(0, times = nProposals), # tally of awards
-  gradeContinuous = rbeta(n = nProposals, shape1 = 10, shape2 = 3)
+  gradeCont = rbeta(n = nProposals, shape1 = 10, shape2 = 3)
 )
 
 # We convert the raw, continuous evaluation "gradeContinuous" into an ordinal
 # variable, with as many equivalence classes as there are possible unique
-# evaluation by the panel (panelGranularity).
+# evaluations by the panel (panelGranularity).
 p$gradeOrd <- findInterval(
-  x = p$gradeContinuous,
-  vec = 0:panelGranularity / panelGranularity
+  x = p$gradeCont,
+  vec = 1:(panelGranularity-1) / panelGranularity
 )
 
 
-# We calculate the resulting ranking position (highest grade means 1st position)
-p$panelRank <- nProposals + 1 - rank(p$panelGrade, ties.method = "min")
-
-
-nBestFundable <- round((panelChoice + lotterySize) * nProposals)
-p$fundable <- p$panelRank <= nBestFundable
-
-nBestExcellent <- round(panelChoice * nProposals)
-p$excellent <- p$panelRank <= nBestExcellent
-
-#$fundable <- p$panelGrade >= thFundable
-#p$excellent <- p$panelGrade >= thExcellent
-p$lotteryPool <- p$fundable & !p$excellent
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# We identify which proposals received a sufficiently high grade for it to be
+# considered for funding. When sufficientMerit is set to 0, it means that all
+# proposals will be eligible: this models a Type 4 lottery, where a panel's 
+# opinion does not matter at all, and all (formally eligibile) proposals make it
+# to the lottery pool.
+p$sufficMeritorious <- p$gradeOrd >= ((panelGranularity - 1) * sufficientMerit) 
+
+
+# We calculate the resulting ranking position (highest grade means 1st
+# position). Note that this is a weak ordering, meaning that proposals
+# with the same grade will be in a tie. For downstream efficiency, here we
+# calculate two alternative ways of coding ties: "floor" and "ceiling".
+p$panelRank_floor <- nProposals - rank(p$gradeOrd, ties.method = "max") + 1
+p$panelRank_ceil <- nProposals - rank(p$gradeOrd, ties.method = "min") + 1
+
+
+# And we initialize some other variables:
+p$panelChoice <- FALSE
+p$lotteryPool <- FALSE
+p$lotteryChoice <- FALSE
+p$funded <- FALSE
+
+
+# Next we calculate how many proposals are to be funded.
+NtoBeFunded <- round(nProposals * targetFundingRate)
+NtoBeDrawn <- round(NtoBeFunded * lotteryChoiceRate)
+
+# And run the lottery.
+p0 <- runType0(p, NtoBeFunded)
+p1 <- runType1(p, NtoBeFunded, weights = NA)
+p2 <- runType2(p, NtoBeFunded, NtoBeDrawn, weights = NA)
+p3 <- runType3(p, NtoBeFunded, weights = NA)
+p4 <- runType4(p, NtoBeFunded, weights = NA)
 
 
 
--- util.r
+++ util.r
@@ -1,6 +1,179 @@
 # This script contains all support functions for the simulation.
 #
+#
 
+
+################################################################################
+################################################################################
+# Lottery types
+
+
+
+# Type 0 _______________________________________________________________________
+# Type 0 lotteries actually involve no lottery at all: funding is entirely
+# determined by the panel ranking. If there are ties for the last funded
+# position, we assume the panel will recommend all tying proposals for
+# funding. (Note: this means that the number of proposals recommended for
+# funding can sometimes exceed the target funding rate).
+runType0 <- function(p, NtoBeFunded, ...) {
+  winners <- p$panelRank_floor <= NtoBeFunded
+  p$won <- p$won + winners # adding to the winners' tally of total wins
+  p$panelChoice <- winners
+  p$funded <- winners
+  
+  return(p)
+}
+
+
+# Type 1 _______________________________________________________________________
+# Type 1 means that a lottery is used to break any tie for the last fundable
+# position(s) in the panel ranking.
+runType1 <- function(p, NtoBeFunded, weights = NA, ...) {
+  
+  # Using the "ceiling" ranking allows me to exclude from the panel choice 
+  # any tie that might arise for the last fundable position(s).
+  p$panelChoice <- p$panelRank_ceil <= NtoBeFunded
+  
+  
+  # Are there ties we need to break with a lottery?
+  NtoBeDrawn <- NtoBeFunded - sum(p$panelChoice)
+  if (NtoBeDrawn > 0) {
+    # Identifying proposals in the tie
+    pool <- which(
+      !p$panelChoice &
+        p$panelRank_floor <= NtoBeFunded
+    )
+    p$lotteryPool[pool] <- TRUE
+    
+    # Drawing winners from the pool.
+    # We declare probability weights so that, unless specified otherwise, the
+    # sampling will be from a uniform distribution.
+    if (is.na(weights)) weights <- rep(1, times = nrow(p))
+    lotteryWinners <- sample(
+      x = pool,
+      size = NtoBeDrawn,
+      replace = FALSE,
+      prob = weights
+    )
+    p$lotteryChoice[lotteryWinners] <- TRUE
+  }
+  
+  
+  # Funded proposals are those chosen by the panel, plus those that won the
+  # lottery:
+  p$funded[p$panelChoice | p$lotteryChoice] <- TRUE
+  p$won <- p$won + p$funded # (tally of wins)
+  
+  return(p)
+}
+
+
+# Type 2 _______________________________________________________________________
+# Type 2 is a mixed lottery with bypass: the panel can choose some proposals,
+# (we assume they'll choose those they liked the most), but some winners will
+# have to be chosen by lot from among the ones deemed to be "fundable".
+#
+runType2 <- function(p, NtoBeFunded, NtoBeDrawn, weights = NA, ...) {
+  
+  # Using the "ceiling" ranking allows me to exclude from the panel choice 
+  # any tie that might arise for the last fundable position(s).
+  # Note that if there are ties that make it impossible for the panel to
+  # choose their bypass quota, then the panel will choose none.
+  p$panelChoice <- p$panelRank_ceil <= NtoBeFunded - NtoBeDrawn
+  
+  # In the event the panel didn't choose their bypass quota, we must update
+  # the number of porposals to be chosen by lot:
+  NtoBeDrawn <- NtoBeFunded - sum(p$panelChoice)
+  
+  
+  # Then we define the lottery pool. It should include:
+  pool <- which(
+    !p$panelChoice & # - proposals that have not already been chosen;
+      p$sufficMeritorious # - proposals found to be sufficiently meritorious.
+  )
+  p$lotteryPool[pool] <- TRUE
+  
+  # Drawing winners from the pool.
+  # We declare probability weights so that, unless specified otherwise, the
+  # sampling will be from a uniform distribution.
+  if (is.na(weights)) weights <- rep(1, times = nrow(p))
+  lotteryWinners <- sample(
+    x = pool,
+    size = NtoBeDrawn,
+    replace = FALSE,
+    prob = weights
+  )
+  p$lotteryChoice[lotteryWinners] <- TRUE
+  
+  # Funded proposals are those chosen by the panel, plus those that won the
+  # lottery:
+  p$funded[p$panelChoice | p$lotteryChoice] <- TRUE
+  p$won <- p$won + p$funded # (tally of wins)
+  
+  return(p)
+}
+
+
+# Type 3 _______________________________________________________________________
+# These are lotteries of all sufficiently meritorious proposals.
+#
+runType3 <- function(p, NtoBeFunded, weights = NA, ...) {
+  
+  # The lottery pool includes all proposals that received a sufficiently
+  # high grade, i.e. the ones that are sufficiently meritorious:
+  pool <- which(p$sufficMeritorious)
+  p$lotteryPool[pool] <- TRUE
+  
+  # Drawing winners from the pool.
+  # We declare probability weights so that, unless specified otherwise, the
+  # sampling will be from a uniform distribution.
+  if (is.na(weights)) weights <- rep(1, times = nrow(p))
+  lotteryWinners <- sample(
+    x = pool,
+    size = NtoBeFunded, ##
+    replace = FALSE,
+    prob = weights
+  )
+  p$lotteryChoice[lotteryWinners] <- TRUE
+  p$funded[lotteryWinners] <- TRUE
+  p$won <- p$won + p$funded # (tally of wins)
+  
+  return(p)
+}
+
+
+# Type 4 _______________________________________________________________________
+# A lottery of this type is a lottery of all proposals -- no evaluation by the
+# panel comes into play, not even in determining the lottery pool.
+#
+runType4 <- function(p, NtoBeFunded, weights = NA, ...) {
+  
+  # All proposals are in the lottery pool:
+  p$lotteryPool <- TRUE
+  
+  # Drawing winners.
+  # We declare probability weights so that, unless specified otherwise, the
+  # sampling will be from a uniform distribution.
+  if (is.na(weights)) weights <- rep(1, times = nrow(p))
+  lotteryWinners <- sample(
+    x = 1:nrow(p),
+    size = NtoBeFunded, ##
+    replace = FALSE,
+    prob = weights
+  )
+  p$lotteryChoice[lotteryWinners] <- TRUE
+  p$funded[lotteryWinners] <- TRUE
+  p$won <- p$won + p$funded # (tally of wins)
+  
+  return(p)
+}
+
+
+
+
+################################################################################
+################################################################################
+# Miscellanea
 
 gini = function (x) { # adapted from ineq::Gini 
   if (any(is.na(x))) {
@@ -18,7 +191,7 @@ gini = function (x) { # adapted from ineq::Gini
 
 
 truncate <- function(x, min = 0, max = 1){
-  if (length(x) > 1) {return(sapply(x, truncate, min = min, max = max))}
+  if (length(x) > 1) return(sapply(x, truncate, min = min, max = max))
   ifelse(
     x < min,
     return(min),
